f2d8d21039c045e826af11d4b36a9a9343fe7be6
gf-espedia <giulia.favaro@espedia.it>
2019-07-18 13:50:28 +0000

    Update in order to work in any portal

--- webapp/controller/TileNotificationCreatorView1.controller.js
+++ webapp/controller/TileNotificationCreatorView1.controller.js
@@ -5,7 +5,7 @@ sap.ui.define([
 
 	return Controller.extend("TileNotificationCreator.TileNotificationCreator.controller.TileNotificationCreatorView1", {
 		onInit: function () {
-			window.location.replace("https://flpnwc-m9a44f3468.dispatcher.hana.ondemand.com/sites/espediasimplemaintenance#Shell-home ");
+			history.back();
 			window.open("https://notificationcreator-m9a44f3468.dispatcher.hana.ondemand.com", '_blank');
 		}
 	});
